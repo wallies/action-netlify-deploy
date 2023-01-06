b50379c3132c3cb001298ca07f3fd877a460e532
Cameron <561860+wallies@users.noreply.github.com>
2023-01-06 13:08:57 +1000

    update-entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,26 @@ npm i -g netlify-cli
 # Save its exec path to run later
 NETLIFY_CLI=$(which netlify)
 
+NODE_VERSION=$9
+
+# We can start using INPUT_ notation instead of passing
+# env variables as arg to docker
+USE_NVM=$INPUT_USE_NVM
+
+# Install node from NVM to honor .nvmrc files
+if [[ $USE_NVM == "true" ]] && ([[ -n $NODE_VERSION ]] || [[ -e ".nvmrc" ]])
+then
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+	[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
+
+	if [[ -n $NODE_VERSION ]]
+	then
+		nvm install "$NODE_VERSION"
+	else
+		nvm install
+	fi
+fi
+
 NETLIFY_AUTH_TOKEN=$1
 NETLIFY_SITE_ID=$2
 NETLIFY_DEPLOY_TO_PROD=$3
@@ -17,47 +37,19 @@ INSTALL_COMMAND=$6
 BUILD_COMMAND=$7
 DEPLOY_ALIAS=$8
 
-# Install node from NVM to honor .nvmrc files
-if [[ -n "$node_version" ]] || [[ -e ".nvmrc" ]]; then
-	echo "Installing NVM"
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-	export NVM_DIR="$HOME/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-	if which node > /dev/null; then
-		INSTALLED_NODE=$(node --version)
-	fi
-
-	if [[ -n "$node_version" ]]; then
-		echo "Installing Node from node_version"
-		nvm install "$node_version"
-	elif [[ -e ".nvmrc" ]]; then
-		NVMRC_NODE=$(cat .nvmrc)
-		if [[ "$INSTALLED_NODE" == "$NVMRC_NODE" ]]; then
-			echo "Installed node and nvmrc node are the same, using installed version"
-			nvm use
-		else
-			echo "Installing Node from .nvmrc"
-			nvm install "$NVMRC_NODE"
-			nvm use
-		fi
-	fi
-fi
-
-
 # Install dependencies
-if [[ -n "$INSTALL_COMMAND" ]]; then
-	echo "Installing dependencies via $INSTALL_COMMAND"
-	eval "$INSTALL_COMMAND"
-elif [[ -f yarn.lock ]]; then
-	echo "Installing dependencies via yarn lockfile"
+if [[ -n $INSTALL_COMMAND ]]
+then
+	eval $INSTALL_COMMAND
+elif [[ -f yarn.lock ]]
+then
 	yarn
 else
-	echo "Installing dependencies via npm"
-	npm i --verbose
+	npm i
 fi
 
 # Build project
-eval "${BUILD_COMMAND:-'npm run build'}"
+eval ${BUILD_COMMAND:-"npm run build"}
 
 # Export token to use with netlify's cli
 export NETLIFY_SITE_ID="$NETLIFY_SITE_ID"
@@ -65,21 +57,34 @@ export NETLIFY_AUTH_TOKEN="$NETLIFY_AUTH_TOKEN"
 
 COMMAND="$NETLIFY_CLI deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=\"$INPUT_NETLIFY_DEPLOY_MESSAGE\""
 
-if [[ $NETLIFY_DEPLOY_TO_PROD == "true" ]]; then
+if [[ $NETLIFY_DEPLOY_TO_PROD == "true" ]]
+then
 	COMMAND+=" --prod"
-elif [[ -n $DEPLOY_ALIAS ]]; then
+elif [[ -n $DEPLOY_ALIAS ]]
+then
 	COMMAND+=" --alias $DEPLOY_ALIAS"
 fi
 
 # Deploy with netlify
 OUTPUT=$(sh -c "$COMMAND")
 
-NETLIFY_OUTPUT="$OUTPUT"
+NETLIFY_OUTPUT=$(echo "$OUTPUT")
 NETLIFY_PREVIEW_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a-zA-Z0-9./?=_-]*') #Unique key: --
 NETLIFY_LOGS_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://app.netlify.com/[a-zA-Z0-9./?=_-]*') #Unique key: app.netlify.com
 NETLIFY_LIVE_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | grep -Eov "netlify.com") #Unique key: don't containr -- and app.netlify.com
 
-echo "::set-output name=NETLIFY_OUTPUT::$NETLIFY_OUTPUT"
-echo "::set-output name=NETLIFY_PREVIEW_URL::$NETLIFY_PREVIEW_URL"
-echo "::set-output name=NETLIFY_LOGS_URL::$NETLIFY_LOGS_URL"
-echo "::set-output name=NETLIFY_LIVE_URL::$NETLIFY_LIVE_URL"
+echo "NETLIFY_OUTPUT<<EOF" >> $GITHUB_ENV
+echo "$NETLIFY_OUTPUT" >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
+
+echo "NETLIFY_PREVIEW_URL<<EOF" >> $GITHUB_ENV
+echo "$NETLIFY_PREVIEW_URL" >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
+
+echo "NETLIFY_LOGS_URL<<EOF" >> $GITHUB_ENV
+echo "$NETLIFY_LOGS_URL" >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
+
+echo "NETLIFY_LIVE_URL<<EOF" >> $GITHUB_ENV
+echo "$NETLIFY_LIVE_URL" >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
