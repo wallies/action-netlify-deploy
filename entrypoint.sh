765913cb30fc294085838b26fb49cda6c50d9908
Cameron <561860+wallies@users.noreply.github.com>
2023-05-25 14:41:22 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,31 +2,11 @@
 
 set -e
 
-# Install netlify globally before NVM to prevent EACCESS issues
+# Install netlify globally
 npm i -g netlify-cli
 
 # Save its exec path to run later
 NETLIFY_CLI=$(which netlify)
-
-NODE_VERSION=$9
-
-# We can start using INPUT_ notation instead of passing
-# env variables as arg to docker
-USE_NVM=$INPUT_USE_NVM
-
-# Install node from NVM to honor .nvmrc files
-if [[ $USE_NVM == "true" ]] && ([[ -n $NODE_VERSION ]] || [[ -e ".nvmrc" ]])
-then
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
-	[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
-
-	if [[ -n $NODE_VERSION ]]
-	then
-		nvm install "$NODE_VERSION"
-	else
-		nvm install
-	fi
-fi
 
 NETLIFY_AUTH_TOKEN=$1
 NETLIFY_SITE_ID=$2
@@ -37,15 +17,21 @@ INSTALL_COMMAND=$6
 BUILD_COMMAND=$7
 DEPLOY_ALIAS=$8
 
-# Install dependencies
-if [[ -n $INSTALL_COMMAND ]]
-then
-	eval $INSTALL_COMMAND
-elif [[ -f yarn.lock ]]
-then
-	yarn
+
+# Run install command
+if [[ -n "${INSTALL_COMMAND}" ]]; then
+  ${INSTALL_COMMAND}
+elif [[ -f yarn.lock ]]; then
+  yarn
 else
-	npm i
+  npm i
+fi
+
+# Run build command
+if [[ -n "${BUILD_COMMAND}" ]]; then
+  ${BUILD_COMMAND}
+else
+  npm run build
 fi
 
 # Build project
@@ -57,12 +43,10 @@ export NETLIFY_AUTH_TOKEN="$NETLIFY_AUTH_TOKEN"
 
 COMMAND="$NETLIFY_CLI deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=\"$INPUT_NETLIFY_DEPLOY_MESSAGE\""
 
-if [[ $NETLIFY_DEPLOY_TO_PROD == "true" ]]
-then
-	COMMAND+=" --prod"
-elif [[ -n $DEPLOY_ALIAS ]]
-then
-	COMMAND+=" --alias $DEPLOY_ALIAS"
+if [[ $NETLIFY_DEPLOY_TO_PROD == "true" ]]; then
+  COMMAND+=" --prod"
+elif [[ -n $DEPLOY_ALIAS ]]; then
+  COMMAND+=" --alias $DEPLOY_ALIAS"
 fi
 
 # Deploy with netlify
